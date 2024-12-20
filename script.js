dac94a5ab65834205ae57cd45f1f76edefa1e3d1
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2024-12-20 15:36:39 -0800

    updated playRound function: rock paper scissors base is functional

--- script.js
+++ script.js
@@ -34,16 +34,15 @@ function humanChoice() {
   );
 
   let result = humanInput.trim().toLowerCase();
-  console.log(typeof humanInput); //if user hits cancel returns object data type, but null itself is a primitive data type. let it be known that this is the first time I have experienced this bug on my own! ^-^
 
   if (result === "") {
     humanInput = prompt("Please, make a choice. Or cancel...loser");
   }
+
+  return result;
   //my logic at this stage: I am checking if the user enters nothing. I thought null would be what to check, but null only applies when a user hits the cancel button. If they are hitting the cancel button, its assumed they dont want to enter anything. So rather, I think I should check if a user hits ok or enter, but doesnt actually enter anything, ie an empty string.
 
   //update: I came up with assigning the humanInput to a result variable that trims and lowercases the input. that way after checking if result is an empty string, all we do is return the result!
-
-  return result;
 }
 // end humanChoice function
 
@@ -52,6 +51,18 @@ function playRound(humanChoice, machineChoice) {
   //scissors beats paper 1
   //paper beats rock 2
   // if humanChoice == rock && machineChoice == scissors. check for a winning solution for the human, otherwise the machine wins.
-
-  console.log(humanChoice, machineChoice);
+  if (humanChoice === machineChoice) {
+    //check if both human and machine chose the same option
+    console.log("It's a tie! try again.");
+  } else if (
+    //checks if the human has won
+    (humanChoice === "rock" && machineChoice === "scissors") ||
+    (humanChoice === "scissors" && machineChoice === "paper") ||
+    (humanChoice === "paper" && machineChoice === "rock")
+  ) {
+    console.log("The human has won! All hail the human!");
+  } else {
+    //if the human didnt win, then the machine won.
+    console.log("The machine has won! Computational power reigns supreme!");
+  }
 }
